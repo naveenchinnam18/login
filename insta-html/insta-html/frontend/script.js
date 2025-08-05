cce79f181bf27e1cea8e469419090c0609770438
naveen78932 <chinnamnaveen601@gmail.com>
2025-08-05 16:15:41 +0530

    add

--- insta-html/insta-html/frontend/script.js
+++ insta-html/insta-html/frontend/script.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const username = document.getElementById('username').value.trim();
   const password = document.getElementById('password').value.trim();
 
-  const response = await fetch('http://localhost:5000/login', {
+  const response = await fetch('https://localhost:5000/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
